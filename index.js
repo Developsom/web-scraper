162012f047826941fad1818f8e3c647f82682237
Developsom <zana@cleancflo.nl>
2022-10-08 02:21:51 +0700

    Asked axios to visit the url which has been saved as a string and .then to save the data as html. console log, tested and worked

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const PORT = 3000
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
+const {response} = require("express");
 
 const app = express()
 
+const url = 'https://www.google.nl/search?q=https://myascentium.com/ApplyNow/CommCredit&ei=_W1AY47rO42Z-Aa2xqOwAw&start=0&sa=N&ved=2ahUKEwjO3eLy3c76AhWNDN4KHTbjCDY4ChDy0wN6BAgBEAQ&biw=1512&bih=791&dpr=2'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        console.log(html)
+    })
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
